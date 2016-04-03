8bc379366da12b74736e57053588af2e1c13821e
Katherine <katgm10@gmail.com>
2016-04-03 14:05:53 -0700

    Changed operator listener

--- app/src/main/java/com/gwc/ax/gwccalculator/MainActivity.java
+++ app/src/main/java/com/gwc/ax/gwccalculator/MainActivity.java
@@ -121,7 +121,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             for (int i = 0; i < 4; i++) {
-                if (findViewById(operatorButtons[i]).isPressed()) {
+                if (findViewById(operatorButtons[i]) == v ) {
                     currentOperation = i;
                 }
             }
